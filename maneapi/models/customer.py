9e7b98091f4cb997ca140adb82da011b6f577867
Neil MacLean <nbmac13@gmail.com>
2023-05-09 10:23:52 -0500

    finished core book 3 chapters

--- maneapi/models/customer.py
+++ maneapi/models/customer.py
@@ -1,10 +1,11 @@
 """Customer class module"""
 from django.db import models
 from django.contrib.auth.models import User
-
+from .style import HairStyle
 
 class Customer(models.Model):
     """Customer model class"""
-    stylist = models.ForeignKey(User, on_delete=models.CASCADE)
+    stylist = models.ForeignKey(User, on_delete=models.CASCADE, related_name='clients')
     name = models.CharField(max_length=50)
     date_created = models.DateField(auto_now=True)
+    style = models.ForeignKey(HairStyle, on_delete=models.CASCADE, related_name='rockers')
